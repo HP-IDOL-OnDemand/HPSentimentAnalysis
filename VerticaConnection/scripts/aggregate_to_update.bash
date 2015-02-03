cc39d26a8aa3698818ee99294f850dd96ba633b7
Carlos A. Henríquez Q <carlos.henriquez@lagunex.com>
2015-02-03 10:07:17 +0100

    refactor multiple scripts into one

--- VerticaConnection/scripts/aggregate_to_update.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-tr '|' ' ' |\
-  awk '{ print "update tweet set aggregate_sentiment = '\''"$2"'\'', aggregate_score = "$3" where id="$1";"  }'
-
---
+++ VerticaConnection/scripts/extract_tbl_files.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+TWEET_FILE=$1 # e.g. id|tweet|es|created_at
+IDOL_FILE=$2 # e.g. either 'id|neutral|score', blank line or 'id|sentiment|topic|score'
+
+AGGREGATE_REGEX="([0-9]+\|\w+\|[^|]+$)|^\s*$" # e.g. id|neutral|score or blank line
+
+grep -vE $AGGREGATE_REGEX $IDOL_FILE > sentiment.tbl # id|sentiment|topic|score
+
+TMP_FILE=`mktemp topcoder.XXXXXX`
+grep -E $AGGREGATE_REGEX $IDOL_FILE | cut -d'|' -f2,3 > $TMP_FILE
+paste -d'|' $TWEET_FILE $TMP_FILE | sed 's/\|$//' > tweet.tbl
+rm $TMP_FILE
+
