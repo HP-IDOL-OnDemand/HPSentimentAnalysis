aa292149df9ca9e3311ba30144bcafcdf67aa215
Carlos A. Henríquez Q <carlos.henriquez@lagunex.com>
2015-02-04 23:54:05 +0100

    add tests for missing methods

--- VerticaConnection/src/test/java/com/lagunex/vertica/VerticaTest.java
+++ VerticaConnection/src/test/java/com/lagunex/vertica/VerticaTest.java
@@ -78,4 +78,104 @@ public class VerticaTest {
             assertNotNull(row.get("total"));
         });
     } 
+
+    @Test
+    public void getSentimentHistogramDuringOneHourOrMore() {
+        LocalDateTime begin = LocalDateTime.of(2015, Month.FEBRUARY, 2, 1, 00);
+        LocalDateTime end = LocalDateTime.of(2015, Month.FEBRUARY, 2, 2, 00);
+        List<Map<String, Object>> result = vertica.getSentimentHistogram(begin, end);
+
+        result.stream().forEach(row -> {
+            assertNotNull(row.get("time"));
+            assertNotNull(row.get("total"));
+        });
+    }
+
+    @Test
+    public void getSentimentHistogramDuringLessThanOneHour() {
+        LocalDateTime begin = LocalDateTime.of(2015, Month.FEBRUARY, 2, 1, 0);
+        LocalDateTime end = LocalDateTime.of(2015, Month.FEBRUARY, 2, 1, 50);
+        List<Map<String, Object>> result = vertica.getSentimentHistogram(begin, end);
+
+        result.stream().forEach(row -> {
+            assertNotNull(row.get("time"));
+            assertNotNull(row.get("total"));
+        });
+    } 
+
+    @Test
+    public void getSentimentTotal() {
+        LocalDateTime begin = LocalDateTime.of(2015, Month.FEBRUARY, 2, 1, 0);
+        LocalDateTime end = LocalDateTime.of(2015, Month.FEBRUARY, 2, 8, 0);
+        List<Map<String, Object>> result = vertica.getSentimentTotal(begin, end);
+
+        assertEquals(16, result.size());
+        result.stream().forEach(row -> {
+            assertNotNull(row.get("label"));
+            assertNotNull(row.get("total"));
+        });
+    }
+
+    @Test
+    public void getTweetsWithAggregate() {
+        LocalDateTime begin = LocalDateTime.of(2015, Month.FEBRUARY, 2, 1, 0);
+        LocalDateTime end = LocalDateTime.of(2015, Month.FEBRUARY, 2, 8, 0);
+
+        for (String aggregate : new String[]{"neutral", "positive", "negative"}) {
+            List<Map<String, Object>> result = vertica.getTweetsWithAggregate(aggregate, begin, end); 
+            assertTrue(result.size()>0);
+            result.stream().forEach(row -> {
+                    assertNotNull(row.get("time"));
+                    assertNotNull(row.get("message")); 
+            });
+        }
+    }
+    
+    @Test
+    public void getTweetsWithSentiment() {
+        LocalDateTime begin = LocalDateTime.of(2015, Month.FEBRUARY, 2, 1, 0);
+        LocalDateTime end = LocalDateTime.of(2015, Month.FEBRUARY, 2, 8, 0);
+
+        String sampleSentiment = "Congrats";
+        List<Map<String, Object>> result = vertica.getTweetsWithSentiment(sampleSentiment, begin, end); 
+        assertTrue(result.size()>0);
+        result.stream().forEach(row -> {
+            assertNotNull(row.get("time"));
+            assertNotNull(row.get("message")); 
+        });
+    }
+
+    @Test
+    public void getTweetsWithTimeInATenMinuteWindow() {
+        String window = "2015-02-02 03:00";
+        
+        List<Map<String, Object>> result = vertica.getTweetsWithTime(window); 
+        assertTrue(result.size()>0);
+        result.stream().forEach(row -> {
+            String time = row.get("time").toString();
+            assertNotNull(time);
+            assertTrue(window.compareTo(time)<=0 && "2015-02-02 03:10".compareTo(time)>0);
+            assertNotNull(row.get("message")); 
+        });
+    }
+
+    @Test
+    public void getTweetsWithTimeInAOneMinuteWindow() {
+        String window = "2015-02-02 01:41";
+        
+        List<Map<String, Object>> result = vertica.getTweetsWithTime(window); 
+        assertTrue(result.size()>0);
+        result.stream().forEach(row -> {
+            String time = row.get("time").toString();
+            assertNotNull(time);
+            assertTrue(window.compareTo(time)<=0 && "2015-02-02 01:42".compareTo(time)>0);
+            assertNotNull(row.get("message")); 
+        });
+    }
+
+    @Test
+    public void getTweetsWithTimeWithInvalidParameter() {
+        String window = "invalid datetime string";
+        assertEquals(0,vertica.getTweetsWithTime(window).size()); 
+    }
 }
