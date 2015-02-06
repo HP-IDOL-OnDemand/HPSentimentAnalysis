4f2b3c111541354da3e11426d39f585592467057
Carlos A. Henríquez Q <carlos.henriquez@lagunex.com>
2015-02-06 21:59:48 +0100

    remove unused imports

--- TwitterCollect/src/test/java/com/lagunex/twitter/TwitterClientTest.java
+++ TwitterCollect/src/test/java/com/lagunex/twitter/TwitterClientTest.java
@@ -5,10 +5,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
