69641fb52c6d06efe3895181cf9028e8bf72927d
Carlos A. Henríquez Q <carlos.henriquez@lagunex.com>
2015-02-13 18:55:38 +0100

    change archer size to represent byte length and not character length

--- VerticaConnection/scripts/create_tables.sql
+++ VerticaConnection/scripts/create_tables.sql
@@ -1,18 +1,21 @@
+-- In Vertica, varchar size is given as byte length, not character length.
+-- We consider 4 bytes per character (worst case in UTF-8).
+-- Therefore 140*4 = 560
 create table tweet
 (	id                  integer         not null primary key,
-	message             varchar(140)    not null,
-        lang                char(2)         not null,
-        created_at          timestamp       not null,
-        aggregate_sentiment varchar(10),
-        aggregate_score     float
+	message             varchar(560)    not null,
+    lang                char(2)         not null,
+    created_at          timestamp       not null,
+    aggregate_sentiment varchar(10),
+    aggregate_score     float
 );
 
 create table sentiment
 (	id        auto_increment primary key,
-        tweet_id  integer not null,
-        sentiment varchar(140),
-        topic     varchar(140),
-        score     float
+    tweet_id  integer not null,
+    sentiment varchar(560),
+    topic     varchar(560),
+    score     float
 );
 
 alter table sentiment 
